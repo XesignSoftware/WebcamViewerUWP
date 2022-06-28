b9d8ae0aea8dbface9b3841cc6aa0d659abbf25c
Sebastian Kassai <dot_felix@outlook.com>
2022-06-28 13:18:23 +0200

    Restore to zero.

--- WebcamViewerUWP/Extensions/BoolExtensions.cs
+++
@@ -1,6 +0,0 @@
-﻿using Windows.UI.Xaml;
-
-public static class BoolExtensions
-{
-    public static Visibility Visibility(this bool value) => value ? Windows.UI.Xaml.Visibility.Visible : Windows.UI.Xaml.Visibility.Collapsed;
-}
